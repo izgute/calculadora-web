1f7bdd0cd8bea7735f187292f98f7ac296258bbd
izgute <izgute@floridauniversitaria.es>
2024-11-27 22:19:45 +0100

    actualizar la calculadora para añadir suma y resta

--- calcultor.php
+++ calcultor.php
@@ -3,5 +3,11 @@ $num1 = $_POST["num1"];
 $num2 = $_POST["num2"];
 $operation = $_POST["operation"];
 switch ($operation) {
+    case "suma":
+        echo "Resultado: " . ($num1 + $num2);
+        break;
+        case "resta":
+        echo "Resultado: " . ($num1 - $num2);
+        break;
 }
 ?>
